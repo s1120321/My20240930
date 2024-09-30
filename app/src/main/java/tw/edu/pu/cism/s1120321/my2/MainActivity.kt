25e9200feacd25ca411e9354c75d852c6e7aa059
s1120321 <yieewenhong@gmail.com>
2024-09-30 16:49:59 +0800

    v2

--- app/src/main/java/tw/edu/pu/cism/s1120321/my2/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/cism/s1120321/my2/MainActivity.kt
@@ -1,6 +1,9 @@
 package tw.edu.pu.cism.s1120321.my2
 
 import android.os.Bundle
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun Roll(v:View){
+        var txv:TextView = findViewById(R.id.txv)
+        txv.text = (1..6).random().toString()
     }
 }
